e971711b18b2874ed9f73372751f41236ecbcf40
JaegerNolte <jaeger.nolte@gmail.com>
2026-08-05 19:59:53 -0500

    Nurgling now spawns in the Jungle and Swamp biomes

--- src/main/java/net/jaeger/oldworldfantasy/event/ModEventBusEvents.java
+++ src/main/java/net/jaeger/oldworldfantasy/event/ModEventBusEvents.java
@@ -4,8 +4,12 @@ import net.jaeger.oldworldfantasy.OldWorldFantasyMod;
 import net.jaeger.oldworldfantasy.entity.ModEntities;
 import net.jaeger.oldworldfantasy.entity.client.NurglingModel;
 import net.jaeger.oldworldfantasy.entity.custom.nurgling.NurglingEntity;
+import net.minecraft.world.entity.SpawnPlacementTypes;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -20,5 +24,11 @@ public class ModEventBusEvents {
     @SubscribeEvent
     public static void registerAttributes(EntityAttributeCreationEvent event) {
         event.put(ModEntities.NURGLING.get(), NurglingEntity.createAttributes().build());
+    }
+
+    @SubscribeEvent
+    public static void registerSpawnPlacements(SpawnPlacementRegisterEvent event) {
+        event.register(ModEntities.NURGLING.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+                Monster::checkMonsterSpawnRules, SpawnPlacementRegisterEvent.Operation.REPLACE);
     }
 }
--- src/main/java/net/jaeger/oldworldfantasy/worldgen/ModBiomeModifiers.java
+++ src/main/java/net/jaeger/oldworldfantasy/worldgen/ModBiomeModifiers.java
@@ -1,21 +1,29 @@
 package net.jaeger.oldworldfantasy.worldgen;
 
 import net.jaeger.oldworldfantasy.OldWorldFantasyMod;
+import net.jaeger.oldworldfantasy.entity.ModEntities;
 import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.biome.MobSpawnSettings;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraftforge.common.world.BiomeModifier;
 import net.minecraftforge.common.world.ForgeBiomeModifiers;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import java.util.List;
+
 public class ModBiomeModifiers {
 
     public static final ResourceKey<BiomeModifier> ADD_LEAD_ORE = registerKey("add_lead_ore");
     public static final ResourceKey<BiomeModifier> ADD_ARCANE_COAL_ORE = registerKey("add_arcane_coal_ore");
+
+    public static final ResourceKey<BiomeModifier> SPAWN_NURGLING = registerKey("spawn_nurgling");
+
 
     public static void bootstrap(BootstrapContext<BiomeModifier> context) {
         var placedFeature = context.lookup(Registries.PLACED_FEATURE);
@@ -32,6 +40,12 @@ public class ModBiomeModifiers {
                 HolderSet.direct(placedFeature.getOrThrow(ModPlacedFeatures.ARCANE_COAL_ORE_PLACED_KEY)),
                 GenerationStep.Decoration.UNDERGROUND_ORES
         ));
+
+
+        context.register(SPAWN_NURGLING, new ForgeBiomeModifiers.AddSpawnsBiomeModifier(
+                HolderSet.direct(biomes.getOrThrow(Biomes.SWAMP), biomes.getOrThrow(Biomes.MANGROVE_SWAMP),
+                                 biomes.getOrThrow(Biomes.JUNGLE), biomes.getOrThrow(Biomes.BAMBOO_JUNGLE), biomes.getOrThrow(Biomes.SPARSE_JUNGLE)),
+                List.of(new MobSpawnSettings.SpawnerData(ModEntities.NURGLING.get(), 25, 3, 5))));
     }
 
     private static ResourceKey<BiomeModifier> registerKey(String name) {
